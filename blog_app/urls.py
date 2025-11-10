77ed297a065fd04916f922fdc9ccc96d7103ac27
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-11-10 23:40:54 +0330

    add detail post

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ListPostBlogView
+from .views import ListPostBlogView, PostDetailDetailView
 
 app_name = "blog_app"
 
 urlpatterns = [
     path("list_post/", ListPostBlogView.as_view(), name='list_post_blog'),
+    path("list_post/detail/<int:pk>/", PostDetailDetailView.as_view(), name='detail_blog_post')
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+from blog_app.models import PostBlog
 
 
 class ListPostBlogView(TemplateView):
     template_name = "partials/blog/blog.html"
+
+
+# class PostDetailDetailView(DetailView):
+#     model = PostBlog
+#     template_name = "partials/blog/detail-post.html"
+
+class PostDetailDetailView(TemplateView):
+    template_name = "partials/blog/detail-post.html"
