ac4296198cdfc7d6cbb6918d5bf8f8e5f148d692
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-11-10 19:09:32 +0330

    add template wellcome and add dir

--- account_app/urls.py
+++ account_app/urls.py
@@ -4,7 +4,8 @@ from .views import (
     LoginPasswordView,
     RequestOtpPhoneView,
     VerifyOypPhoneView,
-    ProfileView
+    ProfileView,
+    WellcomeView
 )
 
 
@@ -15,5 +16,6 @@ urlpatterns = [
     path("login_by_password/", LoginPasswordView.as_view(), name="login_by_password"),
     path("request_otp_phone/", RequestOtpPhoneView.as_view(), name="request_otp_phone"),
     path("verify_otp_phone/", VerifyOypPhoneView.as_view(), name="verify_otp_phone"),
-    path("profile/", ProfileView.as_view(), name="profile")
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path("wellcome/", WellcomeView.as_view(), name='wellcome'),
 ]
--- account_app/views.py
+++ account_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 from account_app.forms import (
     LoginPasswordForm,
@@ -55,3 +56,7 @@ class ProfileView(View):
 
     def get(self, request):
         return render(request, self.template_name)
+
+
+class WellcomeView(TemplateView):
+    template_name = "auth/wellcome.html"
