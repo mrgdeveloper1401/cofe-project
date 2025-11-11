c257befb74ad0d85fe450aae4f34c1d9e2700fe3
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-11-11 13:03:21 +0330

    add template search-product

--- product_app/urls.py
+++ product_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import DetailProductView
+from .views import DetailProductView, ListProductView
 
 
 app_name = "product_app"
 
 urlpatterns = [
-    path("p/<int:id>/", DetailProductView.as_view(), name='detail-product-view')
+    path("p/<int:id>/", DetailProductView.as_view(), name='detail-product-view'),
+    path("search-product/", ListProductView.as_view(), name='list-product')
 ]
--- product_app/views.py
+++ product_app/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 
 class DetailProductView(TemplateView):
     template_name = "partials/product/detail-product.html"
+
+
+class ListProductView(TemplateView):
+    template_name = "partials/product/search-product.html"
